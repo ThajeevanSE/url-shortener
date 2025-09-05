8758e038edbdb9fa54a19b2f3facbe6a6d11b9fc
Thajeevan <161084799+ThajeevanSE@users.noreply.github.com>
2025-09-05 21:05:35 +0530

    updated

---
+++ url-shortener/src/main/java/com/example/url_shortener/domain/models/ShortUrlDto.java
@@ -0,0 +1,13 @@
+package com.example.url_shortener.domain.models;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+/**
+ * DTO for {@link com.sivalabs.urlshortener.domain.entities.ShortUrl}
+ */
+public record ShortUrlDto(Long id, String shortKey, String originalUrl,
+                          Boolean isPrivate, Instant expiresAt,
+                          UserDto createdBy, Long clickCount,
+                          Instant createdAt) implements Serializable {
+}
---
+++ url-shortener/src/main/java/com/example/url_shortener/domain/models/UserDto.java
@@ -0,0 +1,9 @@
+package com.example.url_shortener.domain.models;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link com.sivalabs.urlshortener.domain.entities.User}
+ */
+public record UserDto(Long id, String name) implements Serializable {
+}
