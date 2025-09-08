3329884d52447cf9670718d5a77aac788f92b3a2
Thajeevan <161084799+ThajeevanSE@users.noreply.github.com>
2025-09-08 22:51:47 +0530

    update commit

--- url-shortener/src/main/java/com/example/url_shortener/domain/service/ShortUrlService.java
+++ url-shortener/src/main/java/com/example/url_shortener/domain/service/ShortUrlService.java
@@ -70,7 +70,7 @@ public class ShortUrlService {
         return sb.toString();
     }
 
-
+    @Transactional
     public Optional<ShortUrlDto> accessShortUrl(String shortKey) {
         Optional<ShortUrl> shortUrlOptional = shortUrlRepository.findByShortKey(shortKey);
         if(shortUrlOptional.isEmpty()) {
@@ -80,6 +80,8 @@ public class ShortUrlService {
         if(shortUrl.getExpiresAt() != null && shortUrl.getExpiresAt().isBefore(Instant.now())) {
             return Optional.empty();
         }
+        shortUrl.setClickCount(shortUrl.getClickCount()+1);
+        shortUrlRepository.save(shortUrl);
 
         return shortUrlOptional.map(entityMapper::toShortUrlDto);
     }
